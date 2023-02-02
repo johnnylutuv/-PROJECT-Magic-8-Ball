92b0d2b23408bd93f527ff8618070e52ace1e33e
johnnylutuv <39492696+johnnylutuv@users.noreply.github.com>
2023-02-02 15:52:43 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+var userName = '';
+'Jane' - console.log('${userName}');
+
+let userQuestion = 'What is username?';
+console.log(userQuestion);
+
+
+
+const randomNumber = Math.floor(Math.random() * 8);
+
+var eightBall = '';
+
+if (randomNumber == 1) {
+  console.log('It it certain')
+} else if (randomNumber == 2) {
+  console.log('It it decidely so')
+} else if (randomNumber == 3) {
+  console.log('Reply hazy try again')
+} else if (randomNumber == 4) {
+  console.log('Cannot predict now')
+} else if (randomNumber == 5) {
+  console.log('Do not count on it')
+} else if (randomNumber == 6) {
+  console.log('My sources say no')
+} else if (randomNumber == 7) {
+  console.log('Outlook not so good')
+} else if (randomNumber == 8) {
+  console.log('Signs point to yes')
+}
+
+console.log(eightBall);
